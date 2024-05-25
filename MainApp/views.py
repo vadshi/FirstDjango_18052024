0ea174e26c370e9247efcf207b8fe08535271f91
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-05-25 12:10:45 +0300

    Add template context

--- MainApp/views.py
+++ MainApp/views.py
@@ -21,12 +21,11 @@ items = [
 
 
 def home(request):
-    # text = """
-    # <h1>"Изучаем django"</h1>
-    # <strong>Автор</strong>: <i>Иванов И.П.</i>
-    # """
-    # return HttpResponse(text)
-    return render(request, "index.html")
+    context = {
+        "name": "Петров Иван Николаевич",
+        "email": "my_mail@mail.ru"
+    }
+    return render(request, "index.html", context)
 
 
 def about(request):
