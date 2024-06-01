0bd691523e748e04fa05e6ef9eee99bb393d042d
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-06-01 11:31:46 +0300

    Fix items_list.html

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
-from MainApp.models import Item
+from .models import Item
 from django.core.exceptions import ObjectDoesNotExist
 
 
