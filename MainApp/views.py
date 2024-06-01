25fc322a05eaef4b58644fb6ee1fb4d61b22a0fd
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-06-01 10:59:04 +0300

    Done. M3:task3-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
-
-
-# items = [
-#    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-#    {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-#    {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-#    {"id": 7, "name": "Картофель фри" ,"quantity":0},
-#    {"id": 8, "name": "Кепка" ,"quantity":124},
-# ]
 
 
 def home(request):
@@ -34,15 +25,15 @@ def about(request):
 
 
 def get_item(request, item_id: int):
-    item = next((item for item in items if item['id'] == item_id), None)
-
-    if item is not None:
-        context = {
-            "item": item
-        }
+    """ По указанному id возвращаем элемент из БД"""
+    try:
+        item = Item.objects.get(id=item_id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f"Item with id={item_id} not found")
+    else:
+        context = {"item": item}
         return render(request, "item_page.html", context)
-    return HttpResponseNotFound(f"Item with id={item_id} not found")
-
+    
 
 def get_items(request):
     items = Item.objects.all()
